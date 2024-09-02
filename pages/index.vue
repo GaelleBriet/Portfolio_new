aff1c9d513c9efb7ac202b2b1088a08c3f318e36
GaelleBriet <gaelle.briet@protonmail.com>
2024-09-02 22:10:20 +0200

    prepare main page work details

--- pages/index.vue
+++ pages/index.vue
@@ -1,57 +1,9 @@
 <script setup lang="ts">
-import darkModeImage from "../assets/images/Mode=Dark.png";
-import lightModeImage from "../assets/images/Mode=Light.png";
-
-const colorMode = useColorMode();
-
-const imageSrc = computed(() => {
-  return colorMode.value === "dark" ? darkModeImage : lightModeImage;
-});
+import WorkView from "~/views/WorkView.vue";
+import HeroView from "~/views/HeroView.vue";
 </script>
 
 <template>
-  <div
-    class="hero-section min-h-screen flex items-center sm:justify-starts justify-center w-3/4 mx-auto"
-  >
-    <div class="max-w-3xl sm:w-3/4 mx-auto text-center sm:text-left">
-      <h1
-        class="text-5xl text-basic-darkGray dark:text-basic-mediumGray font-extrabold mb-6 dark:text-white"
-      >
-        Bonjour, Je suis <br />
-        <span class="text-basic-onyx dark:text-basic-white">Gaëlle Briet</span>
-      </h1>
-      <p class="text-xl sm:text-2xl mb-8 text-gray-600 dark:text-gray-300">
-        <span class="text-basic-onyx dark:text-basic-white"
-          >Développeuse web,</span
-        >
-        <br />
-        passionnée par le code, je crée des applications web modernes et
-        intuitives qui font la différence pour les entreprises et leurs
-        utilisateurs. <br />
-        Explorez mes réalisations.
-      </p>
-      <div
-        class="flex flex-col md:flex-row justify-center items-center sm:items-start sm:justify-start"
-      >
-        <ButtonComponent
-          :class-name="'w-4/5 px-6 py-3 text-lg mb-4 sm:mr-4 md:w-2/5 bg-btnGradient text-basic-white dark:text-basic-white justify-center'"
-          :label="'Voir mon C.V.'"
-        />
-        <ButtonComponent
-          :class-name="
-            'w-4/5 px-6 py-3 text-lg mb-4 sm:mr-4 md:w-2/5 bg-light-faintWhite dark:bg-basic-onyx ' +
-            'text-basic-onyx dark:text-basic-darkGray justify-center hover:bg-basic-mediumGray dark:hover:bg-basic-mediumGray'
-          "
-          :label="'Contact'"
-        />
-      </div>
-    </div>
-    <div class="sm:w-1/3 min-h-[300px] flex items-center justify-center">
-      <img
-        alt="profile"
-        :src="imageSrc"
-        class="hidden sm:block w-full h-auto max-w-[300px] object-contain"
-      />
-    </div>
-  </div>
+  <HeroView />
+  <WorkView />
 </template>
---
+++ views/WorkView.vue
@@ -0,0 +1,71 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="work-section min-h-screen w-3/4 mx-auto">
+    <div>
+      <p class="text-4xl font-bold text-basic-onyx">Selected Work</p>
+    </div>
+
+    <div
+      class="project-card rounded-3xl border-light-cardBorder border bg-light-cardBG flex flex-col justify-around sm:justify-starts mb-9 px-6 py-8"
+    >
+      <div class="h-full">
+        <div class="h-16 w-16 mb-4">
+          <img src="../assets/icons/GB.svg" />
+        </div>
+        <div class="mb-4">
+          <p class="mb-4">Project Name</p>
+          <p class="mb-4">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut
+            cupiditate eos exercitationem nisi perferendis quibusdam quidem quo
+            tempore velit. Eius expedita id incidunt magni modi optio porro quae
+            quam tenetur?
+          </p>
+        </div>
+        <div>Download App -></div>
+      </div>
+    </div>
+
+    <div
+      class="project-card rounded-3xl border-light-cardBorder border bg-light-cardBG flex flex-col justify-around sm:justify-starts mb-9 px-6 py-8"
+    >
+      <div class="h-full">
+        <div class="h-16 w-16 mb-4">
+          <img src="../assets/icons/GB.svg" />
+        </div>
+        <div class="mb-4">
+          <p class="mb-4">Project Name</p>
+          <p class="mb-4">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut
+            cupiditate eos exercitationem nisi perferendis quibusdam quidem quo
+            tempore velit. Eius expedita id incidunt magni modi optio porro quae
+            quam tenetur?
+          </p>
+        </div>
+        <div>Download App -></div>
+      </div>
+    </div>
+
+    <div
+      class="project-card rounded-3xl border-light-cardBorder border bg-light-cardBG flex flex-col justify-around sm:justify-starts mb-9 px-6 py-8"
+    >
+      <div class="h-full">
+        <div class="h-16 w-16 mb-4">
+          <img src="../assets/icons/GB.svg" />
+        </div>
+        <div class="mb-4">
+          <p class="mb-4">Project Name</p>
+          <p class="mb-4">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut
+            cupiditate eos exercitationem nisi perferendis quibusdam quidem quo
+            tempore velit. Eius expedita id incidunt magni modi optio porro quae
+            quam tenetur?
+          </p>
+        </div>
+        <div>Download App -></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
