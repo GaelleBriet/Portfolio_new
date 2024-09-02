9799570bc0bb14567e8735f9cdcea565f3009159
GaelleBriet <gaelle.briet@protonmail.com>
2024-09-02 20:20:27 +0200

    hero section large view OK

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,23 @@
+<script setup lang="ts">
+import darkModeImage from "../assets/images/Mode=Dark.png";
+import lightModeImage from "../assets/images/Mode=Light.png";
+
+const colorMode = useColorMode();
+
+const imageSrc = computed(() => {
+  return colorMode.value === "dark" ? darkModeImage : lightModeImage;
+});
+</script>
+
 <template>
   <div
-    class="hero-section min-h-screen flex items-center justify-center px-4 sm:px-6 lg:px-8"
+    class="hero-section min-h-screen flex items-center sm:justify-starts justify-center w-3/4 mx-auto"
   >
-    <div class="max-w-3xl mx-auto text-center">
+    <div class="max-w-3xl sm:w-3/4 mx-auto text-center sm:text-left">
       <h1
-        class="text-5xl sm:text-5xl text-basic-darkGray dark:text-basic-mediumGray font-extrabold mb-6 dark:text-white"
+        class="text-5xl text-basic-darkGray dark:text-basic-mediumGray font-extrabold mb-6 dark:text-white"
       >
-        Bonjour, Je suis
+        Bonjour, Je suis <br />
         <span class="text-basic-onyx dark:text-basic-white">Gaëlle Briet</span>
       </h1>
       <p class="text-xl sm:text-2xl mb-8 text-gray-600 dark:text-gray-300">
@@ -19,20 +30,28 @@
         utilisateurs. <br />
         Explorez mes réalisations.
       </p>
-      <div class="flex flex-col justify-center items-center">
+      <div
+        class="flex flex-col md:flex-row justify-center items-center sm:items-start sm:justify-start"
+      >
         <ButtonComponent
-          :class-name="'w-4/5 px-6 py-3 text-lg mb-4 bg-btnGradient text-basic-white dark:text-basic-white justify-center'"
+          :class-name="'w-4/5 px-6 py-3 text-lg mb-4 sm:mr-4 md:w-2/5 bg-btnGradient text-basic-white dark:text-basic-white justify-center'"
           :label="'Voir mon C.V.'"
         />
         <ButtonComponent
-          :class-name="'w-4/5 px-6 py-3 text-lg mb-4 bg-light-faintWhite dark:bg-basic-onyx text-basic-onyx dark:text-basic-darkGray justify-center'"
+          :class-name="
+            'w-4/5 px-6 py-3 text-lg mb-4 sm:mr-4 md:w-2/5 bg-light-faintWhite dark:bg-basic-onyx ' +
+            'text-basic-onyx dark:text-basic-darkGray justify-center hover:bg-basic-mediumGray dark:hover:bg-basic-mediumGray'
+          "
           :label="'Contact'"
         />
       </div>
     </div>
+    <div class="sm:w-1/3 min-h-[300px] flex items-center justify-center">
+      <img
+        alt="profile"
+        :src="imageSrc"
+        class="hidden sm:block w-full h-auto max-w-[300px] object-contain"
+      />
+    </div>
   </div>
 </template>
-
-<script setup lang="ts">
-// Vous pouvez ajouter ici la logique spécifique au composant si nécessaire
-</script>
