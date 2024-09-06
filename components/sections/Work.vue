80d6375f9b1afce7b7b85300012c1095bc5ed648
GaelleBriet <gaelle.briet@protonmail.com>
2024-09-06 10:38:45 +0200

    fix work section

--- components/sections/Work.vue
+++ components/sections/Work.vue
@@ -11,81 +11,81 @@
     </div>
 
     <div
-      class="project-card rounded-3xl border-light-cardBorder dark:border-dark-cardBorder border bg-light-cardBG dark:bg-dark-black_50 flex flex-col justify-between sm:flex-row mb-9 px-6 py-8 sm:pt-8 sm:pb-0 overflow-hidden"
+      class="project-card rounded-3xl border-light-cardBorder dark:border-dark-cardBorder border bg-light-cardBG dark:bg-dark-black_50 flex flex-col justify-around sm:justify-starts sm:flex sm:flex-row mb-9 px-6 py-8 sm:pt-8 sm:pb-0"
     >
-      <div class="h-full sm:w-2/3 flex flex-col justify-between">
-        <div>
-          <div class="h-16 w-16 mb-4">
-            <img src="../../assets/icons/GB.svg" alt="GB logo" />
-          </div>
-          <div class="mb-4">
-            <p
-              class="mb-4 text-basic-onyx dark:text-basic-white text-xl font-extrabold"
-            >
-              Project Name
-            </p>
-            <p class="mb-4 text-basic-darkGray text-base font-normal">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut
-              cupiditate eos exercitationem nisi perferendis quibusdam quidem
-              quo tempore velit. Eius expedita id incidunt magni modi optio
-              porro quae quam tenetur?
-            </p>
-          </div>
+      <div class="h-full sm:w-2/3">
+        <div class="h-16 w-16 mb-4">
+          <img src="../../assets/icons/GB.svg" alt="project-icon" />
+        </div>
+        <div class="mb-4">
+          <p
+            class="mb-4 text-basic-onyx dark:text-basic-white text-xl font-extrabold"
+          >
+            Project Name
+          </p>
+          <p class="mb-4 text-basic-darkGray text-base font-normal">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut
+            cupiditate eos exercitationem nisi perferendis quibusdam quidem quo
+            tempore velit. Eius expedita id incidunt magni modi optio porro quae
+            quam tenetur?
+          </p>
         </div>
         <div
-          class="text-basic-onyx dark:text-basic-white text-lg font-medium flex items-center mb-4 sm:mb-0"
+          class="text-basic-onyx dark:text-basic-white text-lg font-medium flex items-center"
         >
           <p class="flex items-center cursor-pointer">
-            Download App
-            <Icon name="heroicons:arrow-right" class="h-6 w-6 ms-2" />
+            Download App<Icon
+              name="heroicons:arrow-right"
+              class="h-6 w-6 ms-2"
+            />
           </p>
         </div>
       </div>
-      <div class="sm:w-1/3 hidden sm:flex items-end justify-end">
+      <div class="sm:h-full">
         <img
           src="../../assets/images/iPhone%2014.png"
-          alt="iPhone 14"
-          class="max-h-full object-contain"
+          class="sm:flex items-end"
+          alt="project-img"
         />
       </div>
     </div>
 
     <div
-      class="project-card rounded-3xl border-light-cardBorder dark:border-dark-cardBorder border bg-light-cardBG dark:bg-dark-black_50 flex flex-col justify-between sm:flex-row mb-9 px-6 sm:ps-6 sm:pe-0 py-8 sm:pt-8 sm:pb-0 overflow-hidden"
+      class="project-card rounded-3xl border-light-cardBorder dark:border-dark-cardBorder border bg-light-cardBG dark:bg-dark-black_50 flex flex-col justify-around sm:justify-starts sm:flex sm:flex-row mb-9 px-6 py-8 sm:pt-8 sm:pb-0"
     >
-      <div class="h-full sm:w-2/3 flex flex-col justify-between">
-        <div>
-          <div class="h-16 w-16 mb-4">
-            <img src="../../assets/icons/GB.svg" alt="GB logo" />
-          </div>
-          <div class="mb-4">
-            <p
-              class="mb-4 text-basic-onyx dark:text-basic-white text-xl font-extrabold"
-            >
-              Project Name
-            </p>
-            <p class="mb-4 text-basic-darkGray text-base font-normal">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut
-              cupiditate eos exercitationem nisi perferendis quibusdam quidem
-              quo tempore velit. Eius expedita id incidunt magni modi optio
-              porro quae quam tenetur?
-            </p>
-          </div>
+      <div class="h-full sm:w-2/3">
+        <div class="h-16 w-16 mb-4">
+          <img src="../../assets/icons/GB.svg" alt="project-icon" />
+        </div>
+        <div class="mb-4">
+          <p
+            class="mb-4 text-basic-onyx dark:text-basic-white text-xl font-extrabold"
+          >
+            Project Name
+          </p>
+          <p class="mb-4 text-basic-darkGray text-base font-normal">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut
+            cupiditate eos exercitationem nisi perferendis quibusdam quidem quo
+            tempore velit. Eius expedita id incidunt magni modi optio porro quae
+            quam tenetur?
+          </p>
         </div>
         <div
-          class="text-basic-onyx dark:text-basic-white text-lg font-medium flex items-center mb-4 sm:mb-0"
+          class="text-basic-onyx dark:text-basic-white text-lg font-medium flex items-center"
         >
           <p class="flex items-center cursor-pointer">
-            Download App
-            <Icon name="heroicons:arrow-right" class="h-6 w-6 ms-2" />
+            Download App<Icon
+              name="heroicons:arrow-right"
+              class="h-6 w-6 ms-2"
+            />
           </p>
         </div>
       </div>
-      <div class="sm:w-1/3 hidden sm:block relative">
+      <div class="sm:h-full">
         <img
-          src="../../assets/images/iPad%20&%20Apple%20Pencil.png"
-          alt="iPad & Apple Pencil"
-          class="absolute bottom-0 right-0 max-h-full object-contain"
+          src="../../assets/images/iPhone%2014.png"
+          class="sm:flex items-end"
+          alt="project-img"
         />
       </div>
     </div>
